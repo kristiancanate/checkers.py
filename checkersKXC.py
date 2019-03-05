81ef3f6da9f88c11303e7b3e3ba1177aba47bf7c
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-05 11:21:11 -0500

    nested for loop and modified function to create a grid of squares

--- checkersKXC.py
+++ checkersKXC.py
@@ -2,17 +2,18 @@ from graphics import *
 
 sqSz = 50 #Defines size of squares
 
-def create_tile(tlN):
+def create_tile(tR, tC):
     global sqSz
-    tL = Rectangle(Point(sqSz * (tlN + 1), sqSz),
-                   Point(sqSz * (tlN + 2), sqSz * 2))
+    tL = Rectangle(Point(sqSz * (tR + 1), sqSz * (tC + 1)),
+                   Point(sqSz * (tR + 2), sqSz * (tC + 2)))
     tL.draw(chWin)
 
 chWin = GraphWin("Checkers", sqSz * 10, sqSz * 10) #Draws window
 chWin. setCoords(0, 0, sqSz * 10, sqSz * 10) #Assigns coordinates to window
 
-for i in range(8):
-    create_tile(i)
+for j in range(8):
+    for i in range(8):
+        create_tile(i, j)
 
 chWin.getMouse()
 chWin.close()
