792ba540592e9203cd5d35e41629daf5b2980e5b
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-05 11:18:03 -0500

    added a for loop that works in conjunction with the function to create a row of squares

--- checkersKXC.py
+++ checkersKXC.py
@@ -2,16 +2,17 @@ from graphics import *
 
 sqSz = 50 #Defines size of squares
 
-def create_tile():
+def create_tile(tlN):
     global sqSz
-    tL = Rectangle(Point(sqSz, sqSz),
-                   Point(sqSz * 2, sqSz * 2))
+    tL = Rectangle(Point(sqSz * (tlN + 1), sqSz),
+                   Point(sqSz * (tlN + 2), sqSz * 2))
     tL.draw(chWin)
 
 chWin = GraphWin("Checkers", sqSz * 10, sqSz * 10) #Draws window
 chWin. setCoords(0, 0, sqSz * 10, sqSz * 10) #Assigns coordinates to window
 
-create_tile()
+for i in range(8):
+    create_tile(i)
 
 chWin.getMouse()
 chWin.close()
