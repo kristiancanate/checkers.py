dc0d413ede7f74b14f7c65a6257b241419ef9525
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-05 11:13:26 -0500

    creates custom function to draw a single square

--- checkersKXC.py
+++ checkersKXC.py
@@ -2,11 +2,16 @@ from graphics import *
 
 sqSz = 50 #Defines size of squares
 
+def create_tile():
+    global sqSz
+    tL = Rectangle(Point(sqSz, sqSz),
+                   Point(sqSz * 2, sqSz * 2))
+    tL.draw(chWin)
+
 chWin = GraphWin("Checkers", sqSz * 10, sqSz * 10) #Draws window
 chWin. setCoords(0, 0, sqSz * 10, sqSz * 10) #Assigns coordinates to window
 
-tL = Rectangle(Point(sqSz, sqSz), Point(sqSz * 2, sqSz * 2))
-tL.draw(chWin)
+create_tile()
 
 chWin.getMouse()
 chWin.close()
