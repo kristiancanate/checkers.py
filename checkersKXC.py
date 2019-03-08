838bb4be2cca64850734b2b0fb3ea7e20d11380e
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-08 09:28:57 -0500

    moved modulus operators from function to for loop to simplify the orignal function

--- checkersKXC.py
+++ checkersKXC.py
@@ -2,20 +2,11 @@ from graphics import *
 
 sqSz = 50 #Defines size of squares
 
-def create_tile(tR, tC):
+def create_tile(tR, tC, color):
     global sqSz
     tL = Rectangle(Point(sqSz * (tR + 1), sqSz * (tC + 1)),
                    Point(sqSz * (tR + 2), sqSz * (tC + 2)))
-    if tC % 2 == 0: #even
-        if tR % 2 == 0: #even
-            tL.setFill("red")
-        else: #odd
-            tL.setFill("black")
-    else: #odd
-        if tR % 2 == 1: #odd
-            tL.setFill("red")
-        else: #even
-            tL.setFill("black")
+    tL.setFill(color)
     tL.draw(chWin)
 
 chWin = GraphWin("Checkers", sqSz * 10, sqSz * 10) #Draws window
@@ -23,7 +14,17 @@ chWin. setCoords(0, 0, sqSz * 10, sqSz * 10) #Assigns coordinates to window
 
 for j in range(8):
     for i in range(8):
-        create_tile(i, j)
+        if j % 2 == 0: #even
+            if i % 2 == 0: #even
+                sqCol = "red"
+            else: #odd
+                sqCol = "black"
+        else: #odd
+            if i % 2 == 1: #odd
+                sqCol = "red"
+            else: #even
+                sqCol = "black"
+        create_tile(i, j, sqCol)
 
 chWin.getMouse()
 chWin.close()
